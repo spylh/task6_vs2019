baf0ed9849743796053982997589ac7a48973b83
shigeyuki <20195123@g.aut.ac.jp>
2021-11-16 21:06:48 +0900

    お絵描き課題のやつ

--- task6_vs2019/main.cpp
+++ task6_vs2019/main.cpp
@@ -7,6 +7,7 @@
 #include <HDU/hduError.h>
 #include <HDU/hduMatrix.h>
 
+
 hduVector3Dd gCenterOfStylusSphere, gCenterOfGodSphere, gForce;
 HHD ghHD = HD_INVALID_HANDLE;
 HDSchedulerHandle gSchedulerCallback = HD_INVALID_HANDLE;
@@ -26,15 +27,15 @@ void doGraphicsState() {
 	glEnable(GL_SMOOTH);
 
 	GLfloat lightZeroPosition[] = { 10.0f,4.0f,100.0f,0.0f };
-	GLfloat lightZeroColor[] = { 1.0f,1.0f,1.0f,1.0f };
+	GLfloat lightZeroColor[] = { 0.6f,0.6f,0.6f,1.0f };
 	GLfloat lightOnePosition[] = { -1.0f,-2.0f,-100.0f,0.0f };
 	GLfloat lightOneColor[] = { 0.6f,0.6f,0.6f,1.0f };
 
 	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
 	glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightOnePosition);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightOneColor);
+	glLightfv(GL_LIGHT1, GL_POSITION, lightOnePosition);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightOneColor);
 
 	glEnable(GL_LIGHT0);
 	glEnable(GL_LIGHT1);
@@ -51,26 +52,61 @@ void idle(void) {
 	}
 }
 
-#define SSR 20.0
-#define FSR 60.0
-#define STIFFNESS 0.10
+#define SSR 5
+#define BOARDSIZE 60
+#define STIFFNESS 0.30
 
+#define RES 0.1 //頂点を配置する細かさ
 
-#define EPSILON 0.00001
+int point_index;
+int loop_counter;
+GLdouble line_points[30000][4];
 
 void updateEffectorPosition(void) {
+	int buttonState;
 	double m_currentDistance;
 	hduVector3Dd m_centerToEffector = gCenterOfStylusSphere;
+	gCenterOfGodSphere = gCenterOfStylusSphere;
 
-	m_currentDistance = m_centerToEffector.magnitude();
+	//黒板の範囲内で押し込んだ場合
+	if (m_centerToEffector[2] < SSR && abs(m_centerToEffector[0]) < BOARDSIZE / 2 && abs(m_centerToEffector[1]) < BOARDSIZE / 2) {
+		//力覚フィードバック
+		gCenterOfGodSphere[2] = SSR;
+		m_currentDistance = SSR - m_centerToEffector[2];
+		gForce.set(0.0, 0.0, STIFFNESS * m_currentDistance);
+		//お絵描き
+		hdGetIntegerv(HD_CURRENT_BUTTONS, &buttonState);
+		if (buttonState == 1) {
+			//添字リセット
+			if (point_index >= 30000)
+			{
+				point_index = 0;
+				loop_counter = 29999;
+			}
+			else {
+				++point_index;
+				if(loop_counter  != 29999)loop_counter = point_index;
+			}
+			line_points[point_index][0] = (GLdouble)gCenterOfGodSphere[0];
+			line_points[point_index][1] = (GLdouble)gCenterOfGodSphere[1];
+			line_points[point_index][2] = SSR + 0.01;
+			line_points[point_index][3] = 1;
+		}
+		else if (buttonState == 2){
+			for (int i = 0; i < loop_counter; i++) {
+				if (sqrt(pow(line_points[i][0] - gCenterOfGodSphere[0], 2) + pow(line_points[i][1] - gCenterOfGodSphere[1], 2)) < SSR) {
+					line_points[i][3] = 0;
+				}
+			}
 
-	if (gCenterOfStylusSphere[2]>30 || gCenterOfStylusSphere[2]<30) {
-		gCenterOfGodSphere = gCenterOfStylusSphere;
+		}
+
+	}
+	//そうでない場合
+	else {
 		gForce.set(0.0, 0.0, 0.0);
 	}
-	else if (gCenterOfStylusSphere[2]==30) {
-		gForce.set(0.2,0.2,0.2);
-	}
+
 }
 
 HDCallbackCode HDCALLBACK ContactCB(void* data) {
@@ -87,14 +123,13 @@ HDCallbackCode HDCALLBACK ContactCB(void* data) {
 }
 
 void display() {
+
 	doGraphicsState();
 
-	glDisable(GL_CULL_FACE);
-
 	glPushMatrix();
-	glTranslated(0.0, 0.0, 0.0);
+	glTranslated(0.0,0.0,-BOARDSIZE/2);
 	glColor4d(0.2, 0.8, 0.8, 1.0);
-	glutSolidCube(60);
+	glutSolidCube(BOARDSIZE);
 	glPopMatrix();
 
 	glPushMatrix();
@@ -108,6 +143,15 @@ void display() {
 	glColor4d(1.0, 1.0, 0.0, 1.0);
 	glutWireSphere(SSR, 20, 20);
 	glPopMatrix();
+
+	glColor4d(0.0, 0.0, 0.0, 1.0);
+	if (point_index > -1) {
+		for (int i = 0; i < loop_counter; i++) {
+			glBegin(GL_POINTS);
+			if(line_points[i][3]>0)glVertex3d(line_points[i][0], line_points[i][1], line_points[i][2]);
+			glEnd();
+		}
+	}
 
 	glutSwapBuffers();
 }
